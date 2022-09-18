d2efdce1101df58600ef1bb27fc85ec3430583fe
Gustavo Fernandes <gu.gustavo.f.c@gmail.com>
2022-09-18 03:48:11 -0300

    using ESLINT to correct the code

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,18 +1,21 @@
+import React from 'react';
+import P from 'prop-types';
 import './styles.css';
 
 import { PostCard } from '../PostCard';
 
 export const Posts = ({ posts = [] }) => (
-    <div className="posts">
+  <div className="posts">
     {posts.map((post) => (
-      <PostCard 
-        key={post.id} 
-        title={post.title}
-        body={post.body}
-        id={post.id}
-        cover={post.cover}
-      />
+      <PostCard key={post.id} title={post.title} body={post.body} id={post.id} cover={post.cover} />
     ))}
-    </div>
-
+  </div>
 );
+
+Posts.defaultProps = {
+  posts: [],
+};
+
+Posts.propTypes = {
+  posts: P.array,
+};
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import { useCallback, useEffect, useState } from 'react';
 
 import './styles.css';
 
 import { Posts } from '../../components/Posts';
-import { loadPosts } from '../../utils/load-posts'
+import { loadPosts } from '../../utils/load-posts';
 import { Button } from '../../components/Button';
 import { TextInput } from '../../components/TextInput';
 
@@ -16,7 +17,7 @@ export const Home = () => {
 
   const handleLoadPosts = useCallback(async (page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
-    
+
     setPosts(postsAndPhotos.slice(page, postsPerPage));
     setAllPosts(postsAndPhotos);
   }, []);
@@ -33,50 +34,35 @@ export const Home = () => {
 
     setPosts(posts);
     setPage(nextPage);
-  }
+  };
 
   const handleChange = (e) => {
     const { value } = e.target;
     setSearchValue(value);
-  }
+  };
 
   const noMorePosts = page + postsPerPage >= allPosts.length;
-  const filteredPosts = !!searchValue ?
-    allPosts.filter(post => {
-      return post.title.toLowerCase().includes(
-        searchValue.toLowerCase()
-      );
-    })
+  const filteredPosts = searchValue
+    ? allPosts.filter((post) => {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase());
+      })
     : posts;
 
   return (
     <section className="container">
       <div className="search-container">
-        {!!searchValue && (
-          <h1>Search value: {searchValue}</h1>
-        )}
+        {!!searchValue && <h1>Search value: {searchValue}</h1>}
 
         <TextInput searchValue={searchValue} handleChange={handleChange} />
       </div>
 
-      {filteredPosts.length > 0 && (
-        <Posts posts={filteredPosts} />
-      )}
+      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
 
-      {filteredPosts.length === 0 && (
-        <p>Não existem posts =(</p>
-      )}
+      {filteredPosts.length === 0 && <p>Não existem posts =(</p>}
 
       <div className="button-container">
-        {!searchValue && (
-          <Button
-            text="Load more posts"
-            onClick={loadMorePosts}
-            disabled={noMorePosts}
-          />
-        )}
+        {!searchValue && <Button text="Load more posts" onClick={loadMorePosts} disabled={noMorePosts} />}
       </div>
     </section>
   );
-
-}
+};
